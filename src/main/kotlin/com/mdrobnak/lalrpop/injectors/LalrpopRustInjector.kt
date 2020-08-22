da3cb0f12f1b19c7c7b3659f20777ab771304d88
Mcat12 <newtoncat12@yahoo.com>
2020-08-22 14:24:18 -0400

    Perform very basic Rust language injection

---
+++ src/main/kotlin/com/mdrobnak/lalrpop/injectors/LalrpopRustInjector.kt
@@ -0,0 +1,32 @@
+package com.mdrobnak.lalrpop.injectors
+
+import com.intellij.psi.InjectedLanguagePlaces
+import com.intellij.psi.LanguageInjector
+import com.intellij.psi.PsiLanguageInjectionHost
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.psi.util.parentOfType
+import com.mdrobnak.lalrpop.psi.LalrpopNonterminal
+import com.mdrobnak.lalrpop.psi.LalrpopUseStmt
+import com.mdrobnak.lalrpop.psi.impl.LalrpopActionImpl
+import org.rust.lang.RsLanguage
+
+class LalrpopRustInjector : LanguageInjector {
+    override fun getLanguagesToInject(
+        host: PsiLanguageInjectionHost,
+        places: InjectedLanguagePlaces
+    ) {
+        if (!host.isValidHost || host !is LalrpopActionImpl) {
+            return
+        }
+
+        val imports = PsiTreeUtil.findChildrenOfType(host.containingFile, LalrpopUseStmt::class.java)
+            .joinToString("\n") { it.text }
+        val nonterminal = host.parentOfType<LalrpopNonterminal>()!!
+        val returnType = nonterminal.typeRef?.text ?: nonterminal.nonterminalName
+
+        val prefix = "$imports\nfn main() -> $returnType { "
+        val suffix = " }"
+
+        places.addPlace(RsLanguage, host.code.textRangeInParent, prefix, suffix)
+    }
+}
---
+++ src/main/kotlin/com/mdrobnak/lalrpop/psi/ext/LalrpopAction.kt
@@ -0,0 +1,32 @@
+package com.mdrobnak.lalrpop.psi.ext
+
+import com.intellij.extapi.psi.ASTWrapperPsiElement
+import com.intellij.lang.ASTNode
+import com.intellij.psi.LiteralTextEscaper
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiLanguageInjectionHost
+import com.intellij.psi.impl.source.tree.LeafElement
+import com.intellij.psi.util.elementType
+import com.mdrobnak.lalrpop.psi.LalrpopAction
+import com.mdrobnak.lalrpop.psi.LalrpopTypes
+
+abstract class LalrpopActionMixin(node: ASTNode) : ASTWrapperPsiElement(node), LalrpopAction, PsiLanguageInjectionHost {
+    val code: PsiElement
+        get() {
+            assert(lastChild.elementType == LalrpopTypes.CODE)
+            return lastChild
+        }
+
+    override fun isValidHost(): Boolean = true
+
+    override fun updateText(text: String): PsiLanguageInjectionHost {
+        val valueNode = node.firstChildNode
+        assert(valueNode is LeafElement)
+        (valueNode as LeafElement).replaceWithText(text)
+        return this
+    }
+
+    override fun createLiteralTextEscaper(): LiteralTextEscaper<out PsiLanguageInjectionHost> {
+        return LiteralTextEscaper.createSimple(this)
+    }
+}
