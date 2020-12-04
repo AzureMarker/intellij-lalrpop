d5ec91e87d8b038b6c55c74c34636613f9adea35
Dinu Blanovschi <dinu.blanovschi@criptext.com>
2020-12-04 17:54:11 +0200

    Use intellij-rust to compare resolved types

--- src/main/kotlin/com/mdrobnak/lalrpop/injectors/LpRustActionCodeInjector.kt
+++ src/main/kotlin/com/mdrobnak/lalrpop/injectors/LpRustActionCodeInjector.kt
@@ -5,7 +5,11 @@ import com.intellij.lang.injection.MultiHostRegistrar
 import com.intellij.psi.PsiElement
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.psi.util.parentOfType
-import com.mdrobnak.lalrpop.psi.*
+import com.mdrobnak.lalrpop.psi.LpAlternative
+import com.mdrobnak.lalrpop.psi.LpGrammarDecl
+import com.mdrobnak.lalrpop.psi.LpNonterminal
+import com.mdrobnak.lalrpop.psi.LpSelectedType
+import com.mdrobnak.lalrpop.psi.ext.importCode
 import com.mdrobnak.lalrpop.psi.ext.name
 import com.mdrobnak.lalrpop.psi.ext.selected
 import com.mdrobnak.lalrpop.psi.impl.LpActionImpl
@@ -22,8 +26,7 @@ class LpRustActionCodeInjector : MultiHostInjector {
         }
 
         val codeNode = context.code ?: return
-        val imports = PsiTreeUtil.findChildrenOfType(context.containingFile, LpUseStmt::class.java)
-            .joinToString("\n") { it.text }
+        val imports = context.containingFile.importCode
         val nonterminal = context.parentOfType<LpNonterminal>()!!
         val alternative = context.parentOfType<LpAlternative>()!!
 
--- src/main/kotlin/com/mdrobnak/lalrpop/inspections/WrongInferredTypeInspection.kt
+++ src/main/kotlin/com/mdrobnak/lalrpop/inspections/WrongInferredTypeInspection.kt
@@ -1,10 +1,20 @@
 package com.mdrobnak.lalrpop.inspections
 
 import com.intellij.codeInspection.LocalInspectionTool
+import com.intellij.codeInspection.LocalInspectionToolSession
 import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElementVisitor
+import com.intellij.psi.PsiFileFactory
 import com.mdrobnak.lalrpop.psi.LpNonterminal
 import com.mdrobnak.lalrpop.psi.LpVisitor
+import com.mdrobnak.lalrpop.psi.ext.importCode
+import org.rust.lang.RsLanguage
+import org.rust.lang.core.psi.RsTypeAlias
+import org.rust.lang.core.psi.ext.childrenOfType
+import org.rust.lang.core.resolve.ImplLookup
+import org.rust.lang.core.resolve.knownItems
+import org.rust.lang.core.types.infer.RsInferenceContext
 
 /**
  * Reports 2 kinds of problems on a nonterminal
@@ -12,25 +22,56 @@ import com.mdrobnak.lalrpop.psi.LpVisitor
  * 2. There are 2 different alternatives, without action code, where the inferred types are different
  */
 object WrongInferredTypeInspection : LocalInspectionTool() {
-    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
+    override fun buildVisitor(
+        holder: ProblemsHolder,
+        isOnTheFly: Boolean,
+        session: LocalInspectionToolSession
+    ): PsiElementVisitor {
         return object : LpVisitor() {
             override fun visitNonterminal(nonterminal: LpNonterminal) {
+                if (nonterminal.nonterminalName.nonterminalParams != null) return
+
                 val explicitType = nonterminal.typeRef?.resolveType(listOf())
+
+                if (explicitType == "()") return
 
                 var seenType = explicitType
 
                 nonterminal.alternatives.alternativeList.filter { it.action == null }.forEach { alternative ->
                     val alternativeType = alternative.resolveType(listOf())
 
-                    if (seenType != null && seenType != alternativeType) { // TODO: maybe get the rust plugin to do this comparison because it may be very error-prone by just comparing strings
-                        holder.registerProblem(alternative, "Resolved type of alternative is `$alternativeType`, while expected type `$seenType`")
-                    }
-
                     if (seenType == null) {
                         seenType = alternativeType
+                        return
+                    }
+
+                    if (!sameTypes(
+                            nonterminal.project,
+                            nonterminal.containingFile.importCode,
+                            seenType!!,
+                            alternativeType
+                        )
+                    ) {
+                        holder.registerProblem(
+                            alternative,
+                            "Resolved type of alternative is `$alternativeType`, while expected type `$seenType`"
+                        )
                     }
                 }
             }
         }
+    }
+
+    fun sameTypes(project: Project, importCode: String, type1: String, type2: String): Boolean {
+        val file = PsiFileFactory.getInstance(project)
+            .createFileFromText(RsLanguage, "$importCode\ntype T1 = $type1;\ntype T2 = $type2;")
+        val aliases = file.childrenOfType<RsTypeAlias>();
+
+        if (aliases.size != 2) return false
+
+        return RsInferenceContext(project, ImplLookup.relativeTo(aliases[0]), aliases[0].knownItems).canCombineTypes(
+            aliases[0].declaredType,
+            aliases[1].declaredType
+        )
     }
 }
