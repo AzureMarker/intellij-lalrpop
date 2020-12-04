df4cd036d85eb8141c3b57c37eeaa2ff45adedba
Dinu Blanovschi <dinu.blanovschi@criptext.com>
2020-12-04 18:11:47 +0200

    Fix special cases in LpNonterminalParam.delete()

--- src/main/kotlin/com/mdrobnak/lalrpop/psi/ext/LpNonterminalParam.kt
+++ src/main/kotlin/com/mdrobnak/lalrpop/psi/ext/LpNonterminalParam.kt
@@ -6,6 +6,7 @@ import com.intellij.psi.PsiElement
 import com.mdrobnak.lalrpop.psi.LpElementFactory
 import com.mdrobnak.lalrpop.psi.LpElementTypes
 import com.mdrobnak.lalrpop.psi.LpNonterminalParam
+import com.mdrobnak.lalrpop.psi.LpNonterminalParams
 import org.toml.lang.psi.ext.elementType
 
 open class LpNonterminalParamMixin(node: ASTNode) : ASTWrapperPsiElement(node), LpNonterminalParam {
@@ -24,8 +25,18 @@ open class LpNonterminalParamMixin(node: ASTNode) : ASTWrapperPsiElement(node), 
     }
 
     override fun delete() {
+        // if it is alone, just delete the parent `<...>`
+        when (val parent = this.parent) {
+            is LpNonterminalParams -> if (parent.nonterminalParamList.size == 1) {
+                parent.delete()
+                return
+            }
+        }
+
         // on refactoring(safe-delete), also delete the comma that follows this param
         if (this.nextSibling?.elementType == LpElementTypes.COMMA) this.nextSibling.delete()
+        // or delete the previous comma
+        else if (this.prevSibling?.elementType == LpElementTypes.COMMA) this.prevSibling.delete()
 
         super.delete()
     }
