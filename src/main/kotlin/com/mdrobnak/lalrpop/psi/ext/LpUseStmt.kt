f376d7bbc0fef6d69c5670f2536cac31b69a5f34
Dinu Blanovschi <dinu.blanovschi@criptext.com>
2021-01-07 21:11:10 +0200

    Remove last this.apply

--- src/main/kotlin/com/mdrobnak/lalrpop/psi/ext/LpUseStmt.kt
+++ src/main/kotlin/com/mdrobnak/lalrpop/psi/ext/LpUseStmt.kt
@@ -16,10 +16,11 @@ val PsiFile.importCode: String
 abstract class LpUseStmtMixin(node: ASTNode) : ASTWrapperPsiElement(node), LpUseStmt {
     override fun isValidHost(): Boolean = true
 
-    override fun updateText(text: String): PsiLanguageInjectionHost = apply {
+    override fun updateText(text: String): PsiLanguageInjectionHost {
         val valueNode = importCode
         assert(valueNode is LeafElement)
         (valueNode as LeafElement).replaceWithText(text)
+        return this
     }
 
     override fun createLiteralTextEscaper(): LiteralTextEscaper<out PsiLanguageInjectionHost> =
