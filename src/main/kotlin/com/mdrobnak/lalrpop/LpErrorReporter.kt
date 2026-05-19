6e7eb1d91aa45208fa0c8bcc4f5fef44a74ad3ab
Mark Drobnak <mark.drobnak@gmail.com>
2026-05-19 15:24:29 -0700

    Update dependencies and support IDE 2025.1+ versions

--- src/main/kotlin/com/mdrobnak/lalrpop/LpErrorReporter.kt
+++ src/main/kotlin/com/mdrobnak/lalrpop/LpErrorReporter.kt
@@ -17,16 +17,12 @@ import java.awt.Component
  */
 class LpErrorReporter : ErrorReportSubmitter() {
     // Initialize Sentry as soon as this class is loaded.
-    // Create a Hub instance instead of using the global one in case other plugins use Sentry.
     companion object {
-        val sentryHub: Hub
-
         init {
-            val options = SentryOptions()
-            options.dsn = "https://d561feacd9a344cbaf6c87f073a5ff24@o1040632.ingest.sentry.io/6009680"
-            options.isAttachServerName = false
-
-            sentryHub = Hub(options)
+            Sentry.init { options: SentryOptions ->
+                options.dsn = "https://d561feacd9a344cbaf6c87f073a5ff24@o1040632.ingest.sentry.io/6009680"
+                options.isAttachServerName = false
+            }
         }
     }
 
@@ -40,7 +36,7 @@ class LpErrorReporter : ErrorReportSubmitter() {
     ): Boolean {
         val event = events.firstOrNull() ?: return false
 
-        sentryHub.withScope { scope ->
+        Sentry.withScope { scope ->
             // Gather some basic data
             val sentryEvent = SentryEvent()
             sentryEvent.level = SentryLevel.ERROR
@@ -52,17 +48,12 @@ class LpErrorReporter : ErrorReportSubmitter() {
             sentryEvent.setTag("ide.build", ApplicationInfo.getInstance().build.asString())
             sentryEvent.setTag("ide.lastAction", IdeaLogger.ourLastActionId)
 
-            // IntelliJ is currently hard-coded to only send this type of data
-            if (event.data is AbstractMessage) {
-                sentryEvent.throwable = (event.data as AbstractMessage).throwable
-
-                for (attachment in (event.data as AbstractMessage).allAttachments) {
-                    scope.addAttachment(Attachment(attachment.bytes, attachment.path))
-                }
+            for (attachment in event.attachments) {
+                scope.addAttachment(Attachment(attachment.bytes, attachment.path))
             }
 
             // Send the event and report the result
-            val eventId = sentryHub.captureEvent(sentryEvent)
+            val eventId = Sentry.captureEvent(sentryEvent)
             val reportStatus = if (eventId == SentryId.EMPTY_ID) {
                 SubmittedReportInfo.SubmissionStatus.FAILED
             } else {
